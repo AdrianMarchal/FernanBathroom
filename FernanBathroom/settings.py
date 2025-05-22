aa4db8b4dbee0fbca996fbd93b4cb2f5de35b557
AdrianMarchal <javimasus@gmail.com>
2025-05-22 09:48:35 +0200

    Login para cada usuario Gestion de entrada para los diferentes home Pagina error 403 personalizada

--- FernanBathroom/settings.py
+++ FernanBathroom/settings.py
@@ -96,3 +96,5 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
--- applications/users/views.py
+++ applications/users/views.py
@@ -1,18 +1,23 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.generic import FormView
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
-
+from applications.users.decorators import user_type_required
 from applications.users.forms import UserRegisterForm, LoginForm
+from applications.users.mixins import AnonymousRequiredMixin
 from applications.users.models import User
 
 
 # Create your views here.
 
 
-
+@method_decorator(login_required, name='dispatch')
+@method_decorator(user_type_required('administrador'), name='dispatch')
 class UserRegisterView(FormView):
     template_name = "user/admin/registrarProfesor.html"
     form_class = UserRegisterForm
@@ -29,10 +34,9 @@ class UserRegisterView(FormView):
         return super(UserRegisterView, self).form_valid(form)
 
 
-class LoginView(FormView):
+class LoginView(AnonymousRequiredMixin, FormView):
     template_name = 'user/login.html'
     form_class = LoginForm
-    success_url = "/"
 
     def form_valid(self, form):
         user = authenticate(
@@ -42,11 +46,25 @@ class LoginView(FormView):
         login(self.request, user)
         return super(LoginView,self).form_valid(form)
 
+    def get_success_url(self):
+        user = self.request.user
+        if user.type_user_per == 'administrador':
+            return reverse_lazy('home_app:homeAdmin')
+        elif user.type_user_per == 'conserje':
+            return reverse_lazy('home_app:homeConserje')
+        elif user.type_user_per == 'profesor':
+            return reverse_lazy('home_app:home')
+        else:
+            return reverse_lazy('login')
+
 
 class LogoutView(View):
     def get(self, request, *args, **kwargs):
         logout(request)
 
         return HttpResponseRedirect(
-            reverse("users:login")
+            reverse("users_app:userLogin")
         )
+
+def permission_denied_redirect(request):
+    return render(request, "errors/permission_denied.html")
